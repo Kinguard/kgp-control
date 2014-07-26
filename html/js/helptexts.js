568c21219947ab6e2ac0fb3b2b283f78212fd776
PA Nilsson <pa@openproducts.se>
2014-07-26 22:34:12 +0200

    Added spinner loader Added helptexts Added new states for shutdown and reboot

---
+++ html/js/helptexts.js
@@ -0,0 +1,18 @@
+helptext = {
+	masterpassword_confirmation : "<p>Choose a strong password for encryption of OPI.<br>" +
+			"This password is used to unlock OPI after each system restart</p>"+
+			"<p>Please note that the password is not stored anywhere and " +
+			"<strong>can not be recovered or sent to you by e-mail</strong>.</p>"+
+			"<p>If you are not certian that you can remember it, write it down and store it in a safe location.</p>",
+	username : "Select the username for the first user.<br>" +
+			"This user will also be an administrator for your system",
+	unit_id : "Please enter the serial number received in the confirmation email<br>"+
+			"The number is in the format of<br>12abc345-6def-7ghi-890j-123k45l6789",
+	displayname : "Enter the full name to be displayed, ie 'John Doe'",
+	password_confirmation : "Enter the password for the first user",
+	opiname : "Select the name you wish to use to find your OPI.<br>"+
+				"The full name (FQDN) will be 'opiname'.op-i.me",
+	masterpassword : "Enter the <strong>master password<strong> to unlock OPI<br>"+
+					"(This is the password chosen during setup, not a user password)"
+		
+};
