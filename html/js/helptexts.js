b6c06b3618de633f3cd03cbf2b02c4d821e5fdd2
PA Nilsson <pa@openproducts.se>
2014-08-20 13:36:00 +0200

    Lagt till kryssruta för att kunna spara 'master password' på USB.

--- html/js/helptexts.js
+++ html/js/helptexts.js
@@ -3,7 +3,9 @@ helptext = {
 			"This password is used to unlock OPI after each system restart</p>"+
 			"<p>Please note that the password is not stored anywhere and " +
 			"<strong>can not be recovered or sent to you by e-mail</strong>.</p>"+
-			"<p>If you are not certian that you can remember it, write it down and store it in a safe location.</p>",
+			"<p>If you are not certian that you can remember it, write it down and store it in a safe location</p>"+
+			"<p>You can also select to store your password on a USB stick. If the USB stick is then present on boot, the password will be read from the stick.</p>"+
+			"<p>Please note that USB sticks are not fail safe, so make sure you still have your password even if the USB stick fails.",
 	username : "Select the username for the first user.<br>" +
 			"This user will also be an administrator for your system",
 	unit_id : "Please enter the serial number received in the confirmation email<br>"+
